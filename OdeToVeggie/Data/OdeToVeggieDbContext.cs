c5119fe82eebcaaeaab5009154139a7b71e388e1
[JefferyLWY] <[jefferylau.jl@gmail.com]>
2018-12-25 11:57:25 +0800

    Completed Entity Framework module

---
+++ OdeToVeggie/Data/OdeToVeggieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToVeggie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToVeggie.Data
+{
+    public class OdeToVeggieDbContext : DbContext
+    {
+        public OdeToVeggieDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ OdeToVeggie/Services/SqlRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToVeggie.Data;
+using OdeToVeggie.Models;
+
+namespace OdeToVeggie.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToVeggieDbContext _context;
+
+        public SqlRestaurantData(OdeToVeggieDbContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant;
+        }
+
+        public Restaurant Get(int Id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == Id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants;
+        }
+    }
+}
